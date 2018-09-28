4722d9803a262c037b676ddc96fa11778f8da57e
OWL <owlasylum@gmail.com>
2018-09-28 01:15:13 -0400

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,7 +3,8 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ( {  id, description, amount, createdAt} ) => (
 	<div>
@@ -11,7 +12,11 @@ const ExpenseListItem = ( {  id, description, amount, createdAt} ) => (
 			<h3>{ description }</h3>	
 		</Link>
 	
-		<p>{ amount } - { createdAt }</p>
+		<p>
+		{ numeral( amount / 100 ).format( '$0,0.00' ) } 
+		- 
+		{ moment( createdAt ).format( 'MMMM Do, YYYY' ) }
+		</p>
 		
 	</div>
 );
